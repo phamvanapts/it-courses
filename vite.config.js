1c1a83d79307d763794aba9130ddaf5f16507772
phamvana <72136536+phamvana@users.noreply.github.com>
2024-11-15 17:00:00 +0700

    cập nhật

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/it-courses/', // Thay <it-courses> bằng tên repo của bạn
+  // base: '/it-courses/', // Thay <it-courses> bằng tên repo của bạn
 })
